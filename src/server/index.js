d39898ef2097fdda04bc91c1a8865ed13f637768
Kent Moya <kmoya@kmoya.com>
2023-08-03 16:05:15 -0400

    redirect all production traffic to SSL site

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,16 @@ const rootPath = path.resolve(__dirname, '../../dist');
 
 const app = express();
 
-// app.use(express.static(__dirname + '/dist'));
+// Redirect production requests to SSL site (taken from https://jaketrent.com/post/https-redirect-node-heroku/)
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+}  
+
 app.use(express.static(`${rootPath}/sylvester`));
 app.get('/*', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/dist/index.html'));
