89bb3a6d17d59621d2b7036de45e9f015bffadae
Kent Moya <kmoya@kmoya.com>
2023-09-05 13:35:25 -0400

    cleaned up the audit log detail UI

--- src/client/app/audit-log-page/audit-log-detail-dialog/audit-log-detail-dialog.component.ts
+++ src/client/app/audit-log-page/audit-log-detail-dialog/audit-log-detail-dialog.component.ts
@@ -10,11 +10,17 @@ import { SylvesterApiService } from '../../sylvester-api.service';
   styleUrls: ['./audit-log-detail-dialog.component.scss']
 })
 export class AuditLogDetailDialogComponent implements OnInit {
+  isLoading: boolean = false;
   log!: SylvesterAuditLogDetail;
+  oldData: any | null = null;
+  oldDataKeys: string[] = [];
+  newData: any | null = null;
+  newDataKeys: string[] = [];
 
   constructor (
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private apiService: SylvesterApiService
+    private apiService: SylvesterApiService,
+    public dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -22,13 +28,64 @@ export class AuditLogDetailDialogComponent implements OnInit {
   }
 
   getLogData(): void {
+    this.isLoading = true;
     this.apiService.getAuditLog(this.data).subscribe(auditLog => {
       this.log = auditLog;
+
+      if (this.log.oldData) {
+        this.oldData = this.log.oldData;
+        this.oldDataKeys = Object.keys(this.log.oldData);
+        const idIndex = this.oldDataKeys.findIndex(key => key === '_id' || key === 'id');
+        if (idIndex !== -1) {
+          this.oldDataKeys.splice(idIndex, 1);
+        }
+      }
+      
+      if (this.log.newData) {
+        this.newData = this.log.newData;
+        this.newDataKeys = Object.keys(this.log.newData);
+        const idIndex = this.newDataKeys.findIndex(key => key === '_id' || key === 'id');
+        if (idIndex !== -1) {
+          this.newDataKeys.splice(idIndex, 1);
+        }
+      }
+
+      this.isLoading = false;
     })
   }
 
-  getOldData(): string {
-    const x = JSON.stringify(this.log.oldData, null, ' ');
-    return JSON.stringify(this.log.oldData, null, ' ');
+  getChangedFieldStyle(checkKey: string): string {
+    const newKeyIndex = this.newDataKeys.findIndex(key => key === checkKey);
+    const oldKeyIndex = this.oldDataKeys.findIndex(key => key === checkKey);
+
+    let styleString = '';
+    if (newKeyIndex !== -1) {
+      if (oldKeyIndex === -1) {
+        styleString = 'color: red;'
+      } else {
+        if (this.oldData[checkKey] !== this.newData[checkKey]) {
+          styleString = 'color: red;'
+        }
+      }
+    }
+
+    return styleString;
+  }
+  
+  dataChanged(checkKey: string): boolean {
+    const newKeyIndex = this.newDataKeys.findIndex(key => key === checkKey);
+    const oldKeyIndex = this.oldDataKeys.findIndex(key => key === checkKey);
+
+    if (newKeyIndex !== -1) {
+      if (oldKeyIndex === -1) {
+        return true;
+      } else {
+        if (this.oldData[checkKey] !== this.newData[checkKey]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
