02f4efbfab99b9da84bd61c2a453c441de2029f1
Kent Moya <kmoya@kmoya.com>
2023-09-08 17:19:27 -0400

    preparing for Nelnet Ping integration

--- src/client/environments/environment.prod.ts
+++ src/client/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 
 export const AuthConfig = {
     pingEnvironmentID: '2e6fb850-41fc-4f87-9fbe-18fe1447439c',
-    // pingClientID: 'SylvesterDEV', //'e8e7c06f-8bd3-4554-a4ba-d5b32541cca6',
+    // pingClientID: 'SylvesterDEV',
     pingClientID: 'e8e7c06f-8bd3-4554-a4ba-d5b32541cca6',
     pingRedirectUri: 'https://sylvester.agonconsultingservices.com',
     pingEndSessionUri: 'https://sylvester.agonconsultingservices.com'
