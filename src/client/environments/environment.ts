91c576274c80a42385b4330248075b0718f4790e
Kent Moya <kmoya@kmoya.com>
2023-09-11 14:08:19 -0400

    ping testing

--- src/client/environments/environment.ts
+++ src/client/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
 
 export const AuthConfig = {
     pingEnvironmentID: '2e6fb850-41fc-4f87-9fbe-18fe1447439c',
-    // pingClientID: 'SylvesterDEV',
-    pingClientID: 'e8e7c06f-8bd3-4554-a4ba-d5b32541cca6',
+    pingClientID: 'SylvesterDEV',
+    // pingClientID: 'e8e7c06f-8bd3-4554-a4ba-d5b32541cca6',
     pingRedirectUri: 'http://localhost:4200',
     pingEndSessionUri: 'http://localhost:4200'
 }
